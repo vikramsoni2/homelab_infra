0ef499883d80e0f479e89c108f226360f454d077
Vikram Soni <vikram9880@gmail.com>
2023-10-03 04:41:49 +0200

    sonarr and requestrr

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-#ansible-playbook run.yml -K --ask-vault-pass --become-method=su --tags "essential,user"
-ansible-playbook run.yml -K --ask-vault-pass --tags "dockercore,portainer"
+# ansible-playbook run.yml -K --ask-vault-pass --become-method=su --tags "essential,user"
+# ansible-playbook run.yml -K --ask-vault-pass --tags "system,samba,neofetch"
+ansible-playbook run.yml -K --ask-vault-pass --tags "stack"
+# ansible-playbook run.yml -K --ask-vault-pass --tags "samba,neofetch,pip,dockercore"
 # --become-method=su
 # --tags "mount"
 # -tags "nefetch"
