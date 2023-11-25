5e66f2611715c75273a0cc3401eb0ea15f208b77
Vikram Soni <vikram9880@gmail.com>
2023-11-25 13:41:03 +0100

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-# ansible-playbook run.yml -K --ask-vault-pass --become-method=su --tags "essential,user"
-# ansible-playbook run.yml -K --ask-vault-pass --tags "system,samba,neofetch"
-ansible-playbook run.yml -K --ask-vault-pass --tags "stack"
+ansible-playbook run.yml -K --ask-vault-pass --become-method=su --tags "essential,user"
+ansible-playbook run.yml -K --ask-vault-pass
+# ansible-playbook run.yml -K --ask-vault-pass --tags "mega"
 # ansible-playbook run.yml -K --ask-vault-pass --tags "samba,neofetch,pip,dockercore"
 # --become-method=su
 # --tags "mount"
